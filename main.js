f0351d9a168f4f0d2b3ea677757adfe71771ffe1
arm923i <44763562+arm923i@users.noreply.github.com>
2019-06-01 00:59:18 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+// план зала по рядам общая вместительность 300 мест
+// планов может быть и больше... и разные...
+var cinemaHall1 = {
+    row: [10, 15, 20, 25, 25, 25]
+  },
+  cinemaHallMap = '',
+  cinemaHallCurrentRow = 1;
+
+$.each(cinemaHall1.row, function(row, numberOfSeats) {
+  cinemaHallRow = '';
+  for (i = 1; i <= numberOfSeats; i++) {
+    // собираем ряды
+    cinemaHallRow += '<div class="seat" data-row="' + cinemaHallCurrentRow + '" data-seat="' + i + '">&nbsp;</div>';
+  }
+  //собираем зал с проходами между рядами
+  cinemaHallMap += cinemaHallRow + '<div class="passageBetween">&nbsp;</div>';
+  cinemaHallCurrentRow = cinemaHallCurrentRow + 1;
+});
+
+//заполняем в html зал номер 1
+$('.zal1').html(cinemaHallMap);
+// тут по клику определяем что место выкуплено
+$('.seat').on('click', function(e) {
+  // если первый раз кликнули билет выкупили, 
+  // если повторно значит вернули билет
+  $(e.currentTarget).toggleClass('bay');
+  //показываем сколько билетов выкуплено
+  showBaySeat();
+});
+
+function showBaySeat() {
+  result = '';
+  //ищем все места купленные и показываем список выкупленных мест
+  $.each($('.seat.bay'), function(key, item) {
+    result += '<div class="ticket">Ряд: ' +
+      $(item).data().row + ' Место:' +
+      $(item).data().seat + '</div>';
+  });
+
+  $('.result').html(result);
+}
